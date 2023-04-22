2a8db7beeb0126566734338b38c0da0eefcf74fe
Fkalantar <fatanehkalantar@gmail.com>
2023-04-22 15:22:18 -0700

    Added html

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
+import Weather from "./Weather";
+import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
     <div className="container">
-     <h1>Weather App</h1>
+     <Weather />
      <footer>This project was coded by Fataneh Kalantar and is {" "} <a href="https://github.com/Fkalantar/Weather-Search-Engine"
     target="_blank" rel="noreferrer">open-sourced on GitHub</a></footer>
     </div>
