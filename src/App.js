13cf7f777cf8eddedb3bbaeb312df9cd8f956b91
Fkalantar <fatanehkalantar@gmail.com>
2023-04-22 06:38:11 -0700

    Added the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import React from "react";
 export default function App() {
   return (
     <div className="App">
-     Weather App
+     <h1>Weather App</h1>
+     <footer>This project was coded by Fataneh Kalantar and is {" "} <a href="https://github.com/Fkalantar/Weather-Search-Engine"
+    target="_blank" rel="noreferrer">open-sourced on GitHub</a></footer>
     </div>
     );
 }
