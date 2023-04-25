0fb8b04a72364cc4ba28a25b182cc7542658d25e
Fkalantar <fatanehkalantar@gmail.com>
2023-04-25 02:13:36 -0700

    Added API integration

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App() {
   return (
     <div className="App">
     <div className="container">
-     <Weather />
+     <Weather defaultCity="New York" />
      <footer>This project was coded by Fataneh Kalantar and is {" "} <a href="https://github.com/Fkalantar/Weather-Search-Engine"
     target="_blank" rel="noreferrer">open-sourced on GitHub</a></footer>
     </div>
